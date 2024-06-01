e66f49ff4f5de14257e8d989310fba3954872cc4
ghpancardo <ghpancardo@gmail.com>
2024-05-31 20:41:47 -0600

    eliminar arreglado

--- eliminar.php
+++ eliminar.php
@@ -1,19 +1,45 @@
 <?php
 
-$host = '';
-$user = '';
-$contra = '';
-$bd = '';
+include_once "credenciales.php";
 
-$conexion = new mysqli($host, $user, $contra, $bd);
 
-$numeroCuenta = $_POST['numeroCuenta'];
+// Función para conectar a la base de datos
+function conectarBaseDatos($host, $user, $contra, $bd) {
+    $conexion = new mysqli($host, $user, $contra, $bd);
 
-$consulta = "DELETE FROM alumnos WHERE numeroCuenta = '$numeroCuenta'";
+    if ($conexion->connect_error) {
+        redirigir('error.html');
+    }
 
-$conexion->query($consulta);
+    return $conexion;
+}
 
-header("Location: " . "https://fesarweb2.000webhostapp.com/listar.php");
+// Función para redirigir a una URL específica. Entrada URL. Sin salidas
+function redirigir($url) {
+    header("Location: " . $url);
+    exit();
+}
 
-$conexion->close();
+// Función para eliminar ticket. Entradas ticket y conexión. Sin salidas.
+function eliminarTicket($conexion, $ticket) {
+    $consulta = $conexion->prepare("DELETE FROM tickets WHERE ticket = ?");
+    $consulta->bind_param("s", $ticket);
+    $consulta->execute();
+    $consulta->close();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $ticket = $_POST['ticket'];
+
+    incluirCredenciales();
+
+    $conexion = conectarBaseDatos($host, $user, $contra, $bd);
+
+    eliminarTicket($conexion, $ticket);
+
+    redirigir('listar.php');
+
+    $conexion->close();
+}
+
 ?>
